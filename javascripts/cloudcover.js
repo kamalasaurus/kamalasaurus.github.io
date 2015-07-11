7b6986e17f8a5145c36c4b898230d032e8f83b4f
kamalasaurus <kamalasaurus@gmail.com>
2015-07-10 22:09:30 -0700

    scrolling cloudcover

--- javascripts/cloudcover.js
+++ javascripts/cloudcover.js
@@ -6,12 +6,48 @@ window.CLOUDCOVER = {
   width: null,
   height: null,
 
+  cloud: null,
   clouds: [],
+
+  timer: null,
 
   animateClouds: function() {
     this.clouds.forEach(function(cloud) {
-      cloud.x -= 1;
+      cloud.x -= 4;
     });
+  },
+
+  animate: function() {
+    this.cloudFactory();
+    this.animateClouds();
+
+    this.context.clearRect(0, 0, this.width, this.height);
+
+    var removeIndeces = [];
+
+    var that = this;
+    this.clouds.forEach(function(cloud) {
+      if (cloud.x < -that.cloud.clientWidth) {
+        removeIndex = that.clouds.indexOf(cloud);
+      }
+      that.context.drawImage(that.cloud, cloud.x, cloud.y);
+    });
+
+    removeIndeces.forEach(function(idx) {
+      that.clouds.splice(idx, 1);
+    });
+  },
+
+  cloudFactory: function() {
+    if (Math.random() > 0.96) {
+      var x = this.width;
+      var y = Math.floor(
+                Math.random() * this.height
+              ) - (0.5 * this.cloud.clientHeight);
+
+      var cloud = new window.CLOUD(x, y);
+      this.clouds.push(cloud);
+    }
   },
 
   initialize: function() {
@@ -22,9 +58,14 @@ window.CLOUDCOVER = {
     this.header = header;
     this.context = context;
 
+    this.cloud = new Image(128, 96);
+    this.cloud.src = '/images/cloud.gif';
+
     this.resize();
 
     header.appendChild(canvas);
+
+    this.timer = window.setInterval(this.animate.bind(this), 100);
 
   },
 
@@ -35,18 +76,7 @@ window.CLOUDCOVER = {
 
 }
 
-window.CLOUD = function(x, y, id) {
+window.CLOUD = function(x, y) {
   this.x = x;
   this.y = y;
-  this.id = id;
-
-  this.img = new Image(100, 150);
-  this.img.src = 'data.cloud.gif';
-
-  this.animate = function() {
-    this.x -= 1;
-    // document.querySelector('#' + String(id)).
-  };
-
-
 }
--- javascripts/timeline_of_contents.js
+++ javascripts/timeline_of_contents.js
@@ -1,7 +1,13 @@
 window.onload = initialize;
+window.onresize = resize;
 
 function initialize() {
+  window.CLOUDCOVER.initialize();
   window.REQUEST.get('data/blob.json', printer, true);
+}
+
+function resize() {
+  window.CLOUDCOVER.resize();
 }
 
 function printer(data) {
