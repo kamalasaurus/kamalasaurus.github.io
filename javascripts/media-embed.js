f2bbaabf3476ae2eca8869f67741cfbe1c61fe54
kamalasaurus <kamalasaurus@gmail.com>
2015-07-15 14:27:57 -0700

    styling

--- javascripts/media-embed.js
+++ javascripts/media-embed.js
@@ -1,17 +1,10 @@
 window.MEDIAEMBED = {
-
   format: function() {
-    var mediaArray = this.toArray(document.querySelectorAll('.media-embed'));
-
-    mediaArray.forEach(function(div) {
-      var height = div.children[0].getAttribute('height');
-      div.setAttribute('height', String(height) + 'px');
-      div.setAttribute('style', 'background-color: yellow');
-    });
+    var media = this.toArray(document.querySelectorAll('.media-embed'));
+    
   },
 
-  toArray(list) {
+  toArray: function(list) {
     return Array.prototype.slice.call(list);
   }
-
 }
