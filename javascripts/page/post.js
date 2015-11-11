22753be701c0ae3c53cf830cec1301f2a4a6e624
kamalasaurus <kamalasaurus@gmail.com>
2015-11-10 22:22:34 -0800

    working splash page

--- javascripts/page/post.js
+++ javascripts/page/post.js
@@ -2,9 +2,9 @@ window.onload = initialize;
 window.onresize = resize;
 
 function initialize() {
-  window.CLOUDCOVER.initialize();
-  window.MEDIAEMBED.format();
-  window.POST.initialize();
+  window.CLOUDCOVER ? window.CLOUDCOVER.initialize() : void(0);
+  window.MEDIAEMBED ? window.MEDIAEMBED.format() : void(0);
+  window.POST ? window.POST.initialize() : void(0);
 }
 
 function resize() {
